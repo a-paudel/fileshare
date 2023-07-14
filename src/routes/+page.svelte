1966bf2aaa4fdaec1f9d3fa0156be6c82722182c
a-paudel <ankur@paudel.me>
2023-07-14 19:35:23 +0100

    Add progress bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,29 @@
   import { page } from "$app/stores";
   import { browser } from "$app/environment";
   import { goto } from "$app/navigation";
+  import Axios from "axios";
 
   let dragging = false;
 
-  let fileInput: HTMLInputElement;
   let form: HTMLFormElement;
   async function uploadHandler() {
     // form.submit();
     // submit using fetch
     let data = new FormData(form);
-    let resp = await fetch($page.url, {
-      method: "POST",
-      body: data,
+    let resp = await Axios.post($page.url.toString(), data, {
+      onUploadProgress: (e) => {
+        updateProgress((e.loaded / (e.total ?? 1)) * 100);
+      },
     });
-    // get redirect url
-    let json = await resp.json();
+
+    let json = resp.data;
     let url = json.location as string;
     goto(url);
   }
-  $: formData = $page.form;
-  $: {
-    if (browser && formData && formData.error) {
-      console.log(formData.error);
-    }
+
+  let progressBar: HTMLProgressElement;
+  function updateProgress(value: number) {
+    progressBar.value = value;
   }
 </script>
 
@@ -37,19 +37,24 @@
   <h1>File Share</h1>
 
   <label
-    class="bg-gray-2 w-2/3 p5 rounded-lg h50 relative flex items-center justify-center {dragging
+    class="bg-gray-3 w-2/3 p5 rounded-lg h50 relative flex items-center justify-center {dragging
       ? 'shadow-2xl shadow-blue-2 shadow-inset'
       : ''}"
     on:dragenter={() => (dragging = true)}
     on:dragleave={() => (dragging = false)}
     on:drop={() => (dragging = false)}
   >
-    Choose a file
+    <span class="z10 text-xl font-bold text-dark"> Choose a file </span>
+    <progress
+      max="100"
+      value="50"
+      class="wfull hfull absolute top-0 left-0 z0 m0 p0"
+      bind:this={progressBar}
+    />
     <input
       type="file"
       name="file"
       class="absolute top-0 left-0 h-full w-full opacity-0"
-      bind:this={fileInput}
       on:change={uploadHandler}
     />
   </label>
@@ -57,3 +62,18 @@
     <button type="submit">Upload</button>
   </noscript>
 </form>
+
+<style>
+  progress {
+    -webkit-appearance: none;
+    appearance: none;
+  }
+  ::-webkit-progress-bar {
+    background-color: transparent;
+    border-radius: 0.5rem;
+  }
+  ::-webkit-progress-value {
+    background-color: rgb(59, 130, 246);
+    border-radius: 0.5rem;
+  }
+</style>
