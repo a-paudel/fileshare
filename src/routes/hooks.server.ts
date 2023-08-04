de722f3d1053fb01acef2f8bd445795bf192af07
a-paudel <ankur@paudel.me>
2023-08-04 11:14:14 +0100

    Start from scratch

--- src/routes/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import type { Handle } from "@sveltejs/kit";
-import { DeleteExpiredFiles } from "../tasks/delete_expired";
-
-export const handle: Handle = async ({ event, resolve }) => {
-  await DeleteExpiredFiles();
-
-  let resp = await resolve(event);
-  return resp;
-};
